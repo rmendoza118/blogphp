f7197a35f5826369dd8d79581cff624082de6b1d
luishinojosamamani <luis.hinojosa.mamani@outlook.com>
2023-09-22 11:36:32 -0300

    iniciando

---
+++ views/modules/cabezote.php
@@ -0,0 +1,40 @@
+<header class="row">
+			
+	<!-- LOGOTIPO -->
+
+	<div id="logo" class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+
+	
+		<img src="views/images/logotipo.png" class="img-responsive">
+		<a href="backend" class="btn btn-info" role="button">SISTEMA</a>
+	</div>
+	
+	<!-- BOTONERA MOVIL -->
+	
+	<div id="botoneraMovil" class="navbar-header navbar-inverse">
+
+		<button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target="#botonera">
+
+			<span class="icon-bar"></span>
+    		<span class="icon-bar"></span>
+   			<span class="icon-bar"></span>
+
+		</button>
+	
+	</div>
+
+	<!-- BOTONERA NORMAL -->
+	
+	<nav id="botonera" class="col-lg-9 col-md-9 col-sm-12 col-xs-12 collapse navbar-collapse pull-right">
+				
+		<ul class="nav navbar-nav">
+		<!--<li><a href="backend/" target="_blank">Sistema</a></li>-->
+			<li><a href="#top">Noticias</a></li>
+			<li><a href="#galeria">Galería</a></li>
+			<li><a href="#articulos">Artículos</a></li>
+			<li><a href="#videos">Videos</a></li>
+			<li><a href="#contactenos">Contáctenos</a></li>
+			
+	</nav>
+
+</header>
